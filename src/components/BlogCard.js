befad5d3145cef46a78b797184b867871587eb64
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-08 22:49:21 +0530

    BlogCard start

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const BlogCard = () => {
   return (
-    <div>BlogCard</div>
+    <div className='col-3'>
+     <div className='blog-card'>
+      <div className='card-image'>
+        <img src= "images/blog-1.jpg" className='img-fluid' alt='blog'/>
+      </div>
+      <div className='blog-content'>
+        <p className='date'></p>
+        <h5 className='title'>abc</h5>
+        <p className='desc'></p>
+        <Link to="/" className='button'>Read More</Link>
+      </div>
+      </div> 
+    </div>
 
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import BlogCard from "../components/BlogCard";
 
 const Home = () => {
   return (
@@ -233,10 +234,10 @@ const Home = () => {
       <section className="blog-wrapper py-5 home-wrapper-2">
        <div className="container-xxl">
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
         </div>
        </div>
       </section>
