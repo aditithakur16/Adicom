ae6ab925f47f74a67ae6ca8b99be802af84c320e
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-12 22:50:07 +0530

    Special-product start

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -196,6 +196,33 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="product-wrapper py-5 home-wrapper-2">
+       <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">
+              Featured Collection
+            </h3>
+          </div>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+       </div>
+      </section>
+      <section className="special-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">
+              Special Products
+            </h3>
+          </div>
+          
+          </div>
+        </div>
+      </section>
       <section className="marquee-wrapper py-5">
         <div className="container-xxl">
           <div className="row">
@@ -231,21 +258,6 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section className="product-wrapper py-5 home-wrapper-2">
-       <div className="container-xxl">
-        <div className="row">
-          <div className="col-12">
-            <h3 className="section-heading">
-              Featured Collection
-            </h3>
-          </div>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
-       </div>
       </section>
       <section className="blog-wrapper py-5 home-wrapper-2">
        <div className="container-xxl">
